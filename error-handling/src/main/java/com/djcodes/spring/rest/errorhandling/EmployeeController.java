54a865dd00a7e2f720b4d44536093bd0a90922c9
Dhananjay Singh <dhananjay1204@gmail.com>
2017-12-08 01:10:47 +0530

    vnd errors

--- error-handling/src/main/java/com/djcodes/spring/rest/errorhandling/EmployeeController.java
+++ error-handling/src/main/java/com/djcodes/spring/rest/errorhandling/EmployeeController.java
@@ -1,6 +1,10 @@
 package com.djcodes.spring.rest.errorhandling;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +26,12 @@ public class EmployeeController {
   @PostMapping(value = "/updateSalary/{id}")
   public ResponseEntity<?> updateSalary(@PathVariable int id, @RequestParam Double salary) {
     Employee aEmployee = repository.findOne(id);
+    if (aEmployee == null) {
+      VndErrors vndErrors = new VndErrors("" + id, String.format("employee %s not found", "" + id));
+      HttpHeaders httpHeaders = new HttpHeaders();
+      httpHeaders.setContentType(MediaType.parseMediaType("application/vnd.errors"));
+      return new ResponseEntity(vndErrors, httpHeaders, HttpStatus.NOT_FOUND);
+    }
     aEmployee.setSalary(salary);
     repository.save(aEmployee);
     return ResponseEntity.accepted().build();
@@ -30,6 +40,9 @@ public class EmployeeController {
   @GetMapping(value = "/getsdById/{id}")
   public ResponseEntity<?> getById(@PathVariable int id) {
     Employee aEmployee = repository.findOne(id);
+    if (aEmployee == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
     return ResponseEntity.ok(aEmployee);
   }
 }
