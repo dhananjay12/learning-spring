a6b13c9aaf6965fde26aebf5dde4b45909a555cb
Dhananjay Singh <dhananjay1204@gmail.com>
2019-01-16 01:05:40 +0530

    Added get ascii

--- rest-docs-mock-mvc-example/src/test/java/com/mynotes/spring/EmployeeControllerTest.java
+++ rest-docs-mock-mvc-example/src/test/java/com/mynotes/spring/EmployeeControllerTest.java
@@ -8,13 +8,15 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +25,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,6 +33,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mynotes.spring.model.Employee;
+import com.mynotes.spring.repo.EmployeeRepo;
 
 @ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
 @SpringBootTest(classes = RestDocsApplication.class)
@@ -37,6 +42,9 @@ public class EmployeeControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    EmployeeRepo empRepo;
 
     private MockMvc mockMvc;
 
@@ -48,6 +56,11 @@ public class EmployeeControllerTest {
             .build();
     }
 
+    @AfterEach
+    public void cleanUp() {
+        empRepo.deleteAll();
+    }
+
     @Test
     public void createEmployeeExample() throws Exception {
         Map<String, Object> input = new HashMap<>();
@@ -55,7 +68,8 @@ public class EmployeeControllerTest {
         input.put("email", "jane@asd.asd");
         input.put("salary", 95000.0);
 
-        this.mockMvc.perform(post("/employee").contentType(MediaType.APPLICATION_JSON_UTF8)
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/employee")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
             .content(this.objectMapper.writeValueAsString(input)))
             .andExpect(status().isCreated())
             .andExpect(jsonPath("$.name").value("Jane"))
@@ -64,6 +78,18 @@ public class EmployeeControllerTest {
             .andExpect(jsonPath("$.id").isNumber())
             .andDo(document("create-employee-example", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()), requestFields(getEmployeeFieldDescriptor()), responseFields(getEmployeeFieldDescriptorwithId())));
 
+    }
+
+    @Test
+    public void getEmployeeById() throws Exception {
+        Employee testEmp = createEmployee();
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/employee/{id}", testEmp.getId()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name").value(testEmp.getName()))
+            .andExpect(jsonPath("$.email").value(testEmp.getEmail()))
+            .andExpect(jsonPath("$.salary").value(testEmp.getSalary()))
+            .andExpect(jsonPath("$.id").value(testEmp.getId()))
+            .andDo(document("get-employee-by-id", preprocessResponse(prettyPrint()), pathParameters(parameterWithName("id").description("The id of the employee to be fetched")), responseFields(getEmployeeFieldDescriptorwithId())));
     }
 
     private FieldDescriptor[] getEmployeeFieldDescriptor() {
@@ -84,6 +110,14 @@ public class EmployeeControllerTest {
                     .type(String.class.getSimpleName()),
                 fieldWithPath("salary").description("The salary of the employee")
                     .type(Double.class.getSimpleName()) };
+    }
+
+    private Employee createEmployee() {
+        Employee emp = new Employee();
+        emp.setEmail("test@email.com");
+        emp.setName("john");
+        emp.setSalary(10000.00);
+        return empRepo.save(emp);
     }
 
 }
