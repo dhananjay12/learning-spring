5d0bc7ba1ffa2831cae5a69ca58d589b15bcf27d
dhananjay12 <dhananjay1204@gmail.com>
2019-11-06 23:45:55 +0100

    Added simple controller

---
+++ actuator/src/main/java/com/mynotes/spring/actuator/HelloController.java
@@ -0,0 +1,15 @@
+package com.mynotes.spring.actuator;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello from Controller");
+    }
+
+}
